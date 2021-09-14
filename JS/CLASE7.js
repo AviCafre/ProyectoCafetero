3848871bc8e6d8a91ee6563ebdf49f6331e7fabb
Liza Benitez (Avi) <mar7benitez@gmail.com>
2021-09-13 22:21:05 -0300

    prueba1

---
+++ JS/CLASE7.js
@@ -0,0 +1,6 @@
+var nombreusuario = prompt ('¿como te llamas?')
+alert ('Bienvenid@'+nombreusuario)
+var referencia= prompt ('¿como llegaste a nosotros?')
+var cafefav = prompt('¿cual es tu cafe favorito?')
+var paquetes = '2'
+var frecuencia= prompt ('¿cuantas veces al dia tomas cafe?')
---
+++ JS/CLASE8.js
@@ -0,0 +1,20 @@
+function hamburguesa() {
+    var botonera = document.getElementById ('botonera')
+    if(botonera.style.display=='block') {
+       botonera.style.display = 'none'
+    } else {
+        botonera.style.display = 'block'
+    }
+
+    
+}
+
+function seleccionover(textbold) {
+    var textbold = document.getElementById()
+
+    textbold.style.fontWeight = 'bolder'
+}
+function seleccionout(textnormal) {
+    var textnormal = document.getElementById ()
+    textnormal.style.fontWeight  = 'normal'
+}
